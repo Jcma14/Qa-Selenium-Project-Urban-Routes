250aa316b0e85b6f8cf1f2f8a0c0361fa735f096
Camilo Morales <camilomorales.qa@gmail.com>
2026-02-11 20:04:06 +0800

    Add docstring to data.py for test data configuration
    Added module docstring with attributes for test data.

--- data/data.py
+++ data/data.py
@@ -1,3 +1,19 @@
+"""
+Test data configuration for Urban Routes automated tests.
+
+This module contains all test data used across the test suite,
+including URLs, addresses, contact information, and payment details.
+
+Attributes:
+    urban_routes_url: Base URL of the Urban Routes application
+    address_from: Pickup location address
+    address_to: Destination address
+    phone_number: Phone number for verification
+    card_number: Credit card number for payment
+    card_code: Credit card CVV code
+    message_for_driver: Comment text for the driver
+"""
+
 urban_routes_url = 'https://cnt-c1a077da-9736-4f4a-b7a5-1c4417659b03.containerhub.tripleten-services.com?lng=en'
 address_from = 'East 2nd Street, 601'
 address_to = '1300 1st St'
